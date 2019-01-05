69f39f9f3abeaf89442df99adecdd7791c6b1094
K4masz <k4masz@gmail.com>
2019-01-05 13:32:14 +0100

    kolejny

--- LSFRCudaParralel/kernel.cu
+++ LSFRCudaParralel/kernel.cu
@@ -1,121 +1,305 @@
-
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <iostream>
+#include <chrono>
 
-#include <stdio.h>
+//DEKLARACAJE FUNKCJI
+void initMatrix();
+void fillMatrix();
+void showMatrix();
+void initRegisterState();
+void initUsedRegisterValues();
+void startCalculations();
+void calculateCycle();
+void addToUsedRegisterValues();
+void calculateRegisterState();
+bool calculateCell(bool* matrixRow);
+bool isAlreadyUsedState();
+bool isEveryRegisterStateUsed();
+int parseRegisterState();
+void convertToRegisterState(int decimalNumber);
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+using namespace std;
 
-__global__ void addKernel(int *c, const int *a, const int *b)
+bool** conversionMatrix;
+bool* currentRegisterState;
+bool* usedRegisterValues;
+int flipFlopCount = 0;
+bool maximalCycle = false; //2n-1
+
+long time_start, time_stop;
+
+//FLAGI
+
+bool isEveryRegisterStateUsed()
 {
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
+	bool result = true;
+
+	int n = pow(2, flipFlopCount) - 1;
+
+	for (int i = 1; i <= n; i++)
+		if (usedRegisterValues[i] == false)
+		{
+			result = false;
+			break;
+		}
+
+	return result;
 }
+
+bool isAlreadyUsedState()
+{
+	return usedRegisterValues[parseRegisterState()];
+}
+
+//INIT
+
+void initMatrix()
+{
+	cout << "Podaj wielkoœæ macierzy: ";
+	cin >> flipFlopCount;
+	cout << endl << endl;
+
+	cudaMallocManaged(&conversionMatrix, pow(flipFlopCount, 2) * sizeof(bool));
+
+	//INITIALIZING MATRIX
+	conversionMatrix = new bool *[flipFlopCount];
+	for (int i = 0; i < flipFlopCount; i++)
+	{
+		conversionMatrix[i] = new bool[flipFlopCount];
+	}
+	//INITIALIZING MATRIX - END
+}
+
+void initUsedRegisterValues()
+{
+	int n = pow(2, flipFlopCount); // na 3 bitach jest 8 stanów - nie 7
+	usedRegisterValues = new bool[n]{false};
+	usedRegisterValues[0] = 1;
+}
+
+void initRegisterState()
+{
+	cudaMallocManaged(&currentRegisterState, flipFlopCount * sizeof(bool));
+	currentRegisterState = new bool[flipFlopCount];
+	//currentRegisterState[0] = 1;
+	for (int i = 0; i < flipFlopCount; i++)
+	{
+		currentRegisterState[i] = 0;
+	}
+}
+
+void printRegisterState()
+{
+	for (int i = flipFlopCount-1; i >= 0; i--)
+	{
+		cout << currentRegisterState[i] ;
+	}
+	cout<< endl;
+}
+
+//MATRIX
+
+void fillMatrix()
+{
+	for (int i = 0; i < flipFlopCount; i++)
+	{
+		cout << "Podaj " << i + 1 << " wiersz: ";
+
+		for (int j = 0; j < flipFlopCount; j++)
+		{
+			cin >> conversionMatrix[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void showMatrix()
+{
+	cout << endl;
+
+	for (int i = 0; i < flipFlopCount; i++)
+	{
+		for (int j = 0; j < flipFlopCount; j++)
+		{
+			cout << conversionMatrix[i][j] << " ";
+		}
+
+		cout << endl;
+	}
+}
+
+__global__ void calculateCellParallel(bool* currentRS, bool* nextRS, bool** matrix, int n)
+{
+	int result = 0;
+	
+	for (int i = 0; i < n; i++)
+	{
+		result = result + (matrix[threadIdx.x][i] * currentRS[i]);
+		
+	}
+	
+	 nextRS[threadIdx.x] = (result % 2);
+}
+
+bool calculateCell(bool* matrixRow)
+{
+	int result = 0;
+
+	for (int i = 0; i < flipFlopCount; i++)
+	{
+		result = result + (matrixRow[i] * currentRegisterState[i]);
+	}
+
+	return (result % 2);
+}
+
+void calculateRegisterState()
+{
+	bool* nextRegister = new bool[flipFlopCount];
+
+	for (int i = 0; i < flipFlopCount; i++)
+	{
+		nextRegister[i] = calculateCell(conversionMatrix[i]);
+	}
+
+	currentRegisterState = nextRegister;
+}
+
+void calculateRegisterStateParallel()
+{
+	cout << "START ";
+
+	bool* nextRegisterState;
+
+	cudaMallocManaged(&nextRegisterState, flipFlopCount * sizeof(bool));
+	// cudaMallocManaged(&conversionMatrix, pow(flipFlopCount, 2) * sizeof(bool));
+	// cudaMallocManaged(&currentRegisterState, flipFlopCount * sizeof(bool));
+
+	nextRegisterState = new bool[flipFlopCount]{false};
+
+
+	cout << "GPU";
+	calculateCellParallel <<<1, flipFlopCount>>>(currentRegisterState, nextRegisterState, conversionMatrix,
+	                                             flipFlopCount);
+
+
+	cudaDeviceSynchronize();
+	cout << " STOP ";
+
+	*currentRegisterState = nextRegisterState;
+
+	cudaFree(nextRegisterState);
+	printRegisterState();
+}
+
+//LOGIKA
+
+
+void calculateCycle()
+{
+	//przemno¿yæ macierz przez rejestr - mamy rejestr wynikowy
+	int statesNumber = 0;
+	do
+	{
+		statesNumber++;
+		addToUsedRegisterValues();
+		calculateRegisterStateParallel();
+	}
+	while ((parseRegisterState() != 0) && !isAlreadyUsedState());
+	//sprawdziæ czy rejestr przedstawia zero jeœli jest lub czy jest w u¿ytych (parsowane na liczbe) - jesli tak koniec
+	cout << "liczba stanów w cyklu: " << statesNumber << endl;
+	if (statesNumber == pow(2, flipFlopCount) - 1)
+	{
+		maximalCycle = true;
+	}
+}
+
+int parseRegisterState()
+{
+	//metoda parsuj¹ca
+	int result = 0;
+	for (int i = 0; i < flipFlopCount; i++)
+		if (currentRegisterState[i])
+			result += pow(2, i);
+
+	return result;
+}
+
+void addToUsedRegisterValues()
+{
+	usedRegisterValues[parseRegisterState()] = true;
+}
+
+void findFirstAvailableState()
+{
+	int n = pow(flipFlopCount, 2) - 1;
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (!usedRegisterValues[i])
+		{
+			convertToRegisterState(i);
+			break;
+		}
+	}
+}
+
+void convertToRegisterState(int decimalNumber)
+{
+	int i = 0;
+	do
+	{
+		currentRegisterState[i] = decimalNumber % 2;
+		decimalNumber /= 2;
+		i++;
+	}
+	while (decimalNumber > 0);
+}
+
+void startCalculations()
+{
+	time_start = std::chrono::system_clock::now().time_since_epoch().count();
+
+
+	//Pocz¹tek wszystkich obliczeñ
+	int cyclesNumber = 0;
+	//pocz¹tek liczenia jednego cyklu
+	do
+	{
+		//obecny rejestr idzie do u¿ytych rejestrów
+		findFirstAvailableState();
+		calculateCycle();
+		//koniec pêtli
+		cyclesNumber++;
+	}
+	while (!isEveryRegisterStateUsed());
+
+	cout << endl << "Finalna liczba cykli: " << cyclesNumber << endl;
+
+	if (maximalCycle)
+	{
+		cout << endl << "generuje cykl maksymalny" << endl;;
+	}
+	time_stop = (std::chrono::system_clock::now().time_since_epoch().count());
+	cout << "Algorytm trwa³: " << time_stop - time_start << "ms" << endl;
+}
+
+
+//MAIN
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+	
+	initMatrix();
+	fillMatrix();
+	showMatrix();
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	initRegisterState();
+	initUsedRegisterValues();
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	startCalculations();
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
-
-    return 0;
-}
-
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
+	getchar();
+	return 0;
 }
